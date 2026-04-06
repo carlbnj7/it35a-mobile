c1c761bc166c24954ada59b5bdb34dbb6c24dbe4
carlbnj7 <carlbenedictnj7@gmail.com>
2026-04-06 12:09:05 +0800

    Changes in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,68 +1,26 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonMenu, IonMenuButton, IonMenuToggle, IonPage,  IonRouterOutlet, IonSplitPane, IonTitle,IonToolbar } from "@ionic/react";
-import "./Home.css";
-import { Redirect, Route } from "react-router";
-import { homeOutline, logOutOutline } from "ionicons/icons";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import ExploreContainer from '../components/fabComponents';
+import './Home.css';
+import FabComponent from '../components/fabComponents';
+
 
 const Home: React.FC = () => {
-  const path = [
-    { name: "Home", url: "/app/home", icon: homeOutline },
-  ];
-
   return (
-    <IonPage>
-      <IonSplitPane contentId="main">
-
-        <IonMenu contentId="main">
+     <IonPage>
           <IonHeader>
-            <IonToolbar>
-              <IonTitle>Menu</IonTitle>
-            </IonToolbar>
-          </IonHeader>
+             <IonToolbar>
+                <IonButtons>
+                   <IonMenuButton></IonMenuButton>
+                </IonButtons>
+                <IonTitle>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
 
-          <IonContent>
-            {path.map((item, index) => (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem routerLink={item.url} routerDirection="none" lines="none">
-                  <IonIcon slot="start" icon={item.icon} />
-                  <IonLabel>{item.name}</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-            ))}
-
-            <IonMenuToggle autoHide={false}>
-              <IonItem lines="none" button onClick={() => console.log("Logout")}>
-                <IonIcon slot="start" icon={logOutOutline} />
-                <IonLabel>Logout</IonLabel>
-              </IonItem>
-            </IonMenuToggle>
-          </IonContent>
-        </IonMenu>
-
-        <IonRouterOutlet id="main">
-          <Route exact path="/app/home">
-            <IonPage>
-              <IonHeader>
-                <IonToolbar>
-                  <IonButtons slot="start">
-                    <IonMenuButton />
-                  </IonButtons>
-                  <IonTitle>Home</IonTitle>
-                </IonToolbar>
-              </IonHeader>
-              <IonContent className="ion-padding">
-                <h2>Welcome Home!</h2>
-              </IonContent>
-            </IonPage>
-          </Route>
-
-          <Route exact path="/app">
-            <Redirect to="/app/home" />
-          </Route>
-        </IonRouterOutlet>
-
-      </IonSplitPane>
-    </IonPage>
+        </IonContent>
+     </IonPage>
   );
+
 };
 
 export default Home;
